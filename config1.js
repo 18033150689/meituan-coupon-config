170ed4cf955c5c87a795cb005b8bec955eb23658
18033150689 <3946905217@qq.com>
2026-03-10 12:26:24 +0800

    Update config.js - 2026/3/10 12:26:24

--- config1.js
+++ config1.js
@@ -1,12 +1,12 @@
 // 美团外卖红包配置文件
-// 自动生成时间：2026/3/10 12:17:09
+// 自动生成时间：2026/3/10 12:26:23
 
 const siteConfig = {
-  "siteTitle": "饿了么外卖红包",
-  "bgColor": "#3f95ff",
+  "siteTitle": "美团外卖红包",
+  "bgColor": "#FEDB03",
   "header": {
     "enabled": true,
-    "logoUrl": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6RCU/1308X1280/565df7546323c43a291c5f6edb92f35c.jpg/webp",
+    "logoUrl": "",
     "text": "美团",
     "textColor": "#000000",
     "fontSize": "24px",
@@ -17,16 +17,22 @@ const siteConfig = {
   },
   "carousel": [
     {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/6Bzm/750X650/%E5%93%88%E6%A0%B9%E8%BE%BE%E6%96%AF.jpeg/webp",
-      "title": "哈根达斯",
-      "subtitle": "满50减8可叠加！",
-      "link": "https://mp.weixin.qq.com/s/Hv918eS5p5dRwvHFF6tGuw"
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/txWt/1344X704/11.jpg",
+      "title": "水果会场",
+      "subtitle": "领神券59-20、69-25、99-30，果切9.9起",
+      "link": "http://dpurl.cn/EAWMzcQz"
     },
     {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/xPAO/750X650/%E4%B9%90%E4%B9%90%E8%8C%B6.jpeg/webp",
-      "title": "乐乐茶",
-      "subtitle": "满30减8可叠加",
-      "link": "https://mp.weixin.qq.com/s/4W8U2asGg8umfGWJ_1O6-g"
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/cIGA/1344X704/22.jpg",
+      "title": "38大促综合会场",
+      "subtitle": "通用神券49-12/79-18/139-35/300-60",
+      "link": "http://dpurl.cn/mQst03sz"
+    },
+    {
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/2GEq/1344X704/33.jpg",
+      "title": "38大促美妆会场",
+      "subtitle": "领美妆神券最高减300，，3折起再叠券",
+      "link": "http://dpurl.cn/Qyhn1oaz"
     }
   ],
   "stepsType": "image",
@@ -34,38 +40,38 @@ const siteConfig = {
   "stepsColor": "#FF6B00",
   "mainButton": {
     "text": "天天领红包",
-    "color": "#005aad",
+    "color": "135deg, #FF6B00, #FF9800",
     "textColor": "#FFFFFF",
-    "link": "https://mp.weixin.qq.com/s/Rb0a2l7D9A3buMtXzw0Zjg"
+    "link": "http://dpurl.cn/vE8REkUz"
   },
   "subButtonsEnabled": true,
   "subButtons": {
     "button1": {
-      "text": "消费日专属",
-      "desc": "城市大额红包城市通用消费红包",
-      "link": "https://mp.weixin.qq.com/s/kY_6eWWm577-2f4pL-01Wg"
+      "text": "吃喝玩乐 每日福利",
+      "desc": "内部优惠3折起",
+      "link": "http://dpurl.cn/WsChToCz"
     },
     "button2": {
-      "text": "限时快闪",
-      "desc": "最高领18元红包",
-      "link": "https://mp.weixin.qq.com/s/Qt2I5Ix1-N7zwkjXbHLCzQ"
+      "text": "开学季",
+      "desc": "抽免单最高18，领最高减10",
+      "link": "http://dpurl.cn/wHbDPoNz"
     }
   },
   "imageButtonsEnabled": true,
   "imageButtons": [
     {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/vSJg/750X651/%E6%B1%89%E5%A0%A1%E7%8E%8B.jpeg/webp",
-      "text": "满40减6可叠加",
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/kASd/1152X896/44.jpeg",
+      "text": "免费领取",
       "link": "http://dpurl.cn/2IPwHM1z"
     },
     {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/jRq4/750X649/%E5%8D%8E%E8%8E%B1%E5%A3%AB.jpeg/webp",
-      "text": "满25减4可叠加",
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/WNVe/1152X896/66.jpeg",
+      "text": "免费领取",
       "link": "http://dpurl.cn/YzzSTMIz"
     },
     {
-      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/kPLQ/750X648/%E6%B0%B8%E5%92%8C%E5%A4%A7%E7%8E%8B.jpeg/webp",
-      "text": "满30减8可叠加",
+      "image": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/jU0S/1152X896/55.jpeg",
+      "text": "免费领取",
       "link": "http://dpurl.cn/HMGWpnKz"
     }
   ],
@@ -76,7 +82,7 @@ const siteConfig = {
     "4. 美团一天可领3次，饿了么可以领两个(外卖红包和加餐红包)"
   ],
   "redirectBaseUrl": "",
-  "stepsImage": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260307/dl4F/626X135/1.png/webp",
+  "stepsImage": "https://test.fukit.cn/autoupload/f/90B1BiV42O9JOwrrDNqX99iO_OyvX7mIgxFBfDMDErs/20260306/1rW9/626X135/22222222.png/webp",
   "wxSdkConfig": {
     "appId": "wx38215d5cdbb0be8c",
     "timestamp": 1773091937,
